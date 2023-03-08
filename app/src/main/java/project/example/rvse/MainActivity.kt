02c27c1353eb3e2ffb24139b474d57cac2302ac7
Andestand <110184532+Andestand@users.noreply.github.com>
2023-03-08 23:43:09 +0300

    Update MainActivity.kt

--- app/src/main/java/project/example/rvse/MainActivity.kt
+++ app/src/main/java/project/example/rvse/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun init() {
-        binding.recyclerview.layoutManager = GridLayoutManager(this, 1)
+        binding.recyclerview.layoutManager = GridLayoutManager(this, 3)
         binding.recyclerview.adapter = adapter
 
         val tracker = SelectionTracker.Builder(
